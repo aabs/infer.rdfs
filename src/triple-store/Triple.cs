82b0d4fcbaba930a130dba6af97f1f36803b9017
Andrew Matthews <matthews.andrew@gmail.com>
2019-11-01 08:45:52 +1100

    basic compression and duplicate prevention

--- src/triple-store/Triple.cs
+++ src/triple-store/Triple.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 
 namespace triple_store
 {
-    public class Triple
+    public class Triple : IEquatable<Triple>
     {
         private static UriRegistry defaultIndex = new UriRegistry();
         private UriRegistry effectiveIndex = new UriRegistry();
@@ -38,6 +40,24 @@ namespace triple_store
         }
         public (int, int, int) Get()
             => (_subject, _predicate, _object);
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Triple);
+        }
+
+        public bool Equals([AllowNull] Triple other)
+        {
+            return other != null &&
+                   _subject == other._subject &&
+                   _predicate == other._predicate &&
+                   _object == other._object;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(_subject, _predicate, _object);
+        }
 
         int _subject;
         public Uri Subject
@@ -76,6 +96,16 @@ namespace triple_store
             {
                 _object = effectiveIndex.Add(value);
             }
+        }
+
+        public static bool operator ==(Triple left, Triple right)
+        {
+            return EqualityComparer<Triple>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(Triple left, Triple right)
+        {
+            return !(left == right);
         }
     }
 }
